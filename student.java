9c1579cce5ffc4a8c78fe51b3e50d2f8c51ee4dc
EC2 Default User <ec2-user@ip-172-31-40-118.ap-south-1.compute.internal>
2024-08-10 07:29:55 +0000

    commit

---
+++ student.java
@@ -0,0 +1,17 @@
+package com.student.payments;
+
+public class App
+
+
+{
+	public static void main(Strings[]args)
+	{
+		sytem.out.println("welcome to portal");
+		sytem.out.println("student login");
+		sytem.out.println("enter student id");
+		sytem.out.println("enter password");
+	}
+}
+
+
+
