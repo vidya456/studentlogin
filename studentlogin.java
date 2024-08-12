4da808a294cdf7b1a6bf3c7bfc14b054893e0293
EC2 Default User <ec2-user@ip-172-31-38-232.ap-south-1.compute.internal>
2024-08-12 07:14:37 +0000

    frist commit

---
+++ studentlogin.java
@@ -0,0 +1,19 @@
+package com.student.login;
+
+public class App
+
+{
+
+public static void main(String[] args)
+
+{
+
+System.out.println( "Welcome to stuportal");
+
+System.out.println( "Student Login Page");
+
+System.out.println( "Enter student Id:" );
+
+System.out.println( "Enter Password:" );
+}
+}
